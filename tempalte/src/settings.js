fe48dc7f4ee67bcad41d9c7fd0fd7e723860051e
zhaojingqi <2609195880@qq.com>
2019-12-26 16:19:47 +0800

    [update] 模版更换

--- tempalte/src/settings.js
+++ tempalte/src/settings.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: 
+ * @Author: zjq
+ * @Date: 2019-12-21 15:11:13
+ * @LastEditors  : zjq
+ * @LastEditTime : 2019-12-26 16:04:44
+ */
 module.exports = {
 
   title: "{{ name }}",
